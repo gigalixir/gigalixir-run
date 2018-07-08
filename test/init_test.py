d1459567c915506942bb55cf88ebe4b7621ee81a
Jesse <jesse@gigalixir.com>
2018-07-08 11:14:29 -0700

    test_custom_vmargs passes.

--- test/init_test.py
+++ test/init_test.py
@@ -377,7 +377,7 @@ def test_distillery_init(mock_tarfile, mock_os, mock_subprocess, mock_get, mock_
 @mock.patch('tarfile.open')
 @httpretty.activate
 def test_custom_vmargs(mock_tarfile, mock_os, mock_subprocess, mock_get, mock_open):
-    repo = "my_custom_vmargs_app"
+    mock_os.path.isfile.return_value = True
     stdin = mock.Mock(name='stdin')
     mock_subprocess.PIPE = stdin
 
@@ -395,14 +395,40 @@ def test_custom_vmargs(mock_tarfile, mock_os, mock_subprocess, mock_get, mock_op
     with runner.isolated_filesystem():
         os.environ['HOME'] = '.'
         os.environ['APP_KEY'] = 'fake-app-key'
+        my_env['PORT'] = '4000'
+        
+        # set once for Click and once for gigalixir_run
+        os.environ['LOGPLEX_TOKEN'] = 'fake-logplex-token'
+        os.environ['ERLANG_COOKIE'] = 'fake-cookie'
+        os.environ['MY_POD_IP'] = '1.2.3.4'
         my_env['LOGPLEX_TOKEN'] = 'fake-logplex-token'
         my_env['ERLANG_COOKIE'] = 'fake-cookie'
         my_env['MY_POD_IP'] = '1.2.3.4'
-        my_env['PORT'] = '4000'
 
-        result = runner.invoke(gigalixir_run.cli, ['init', repo, 'foreground'])
+        result = runner.invoke(gigalixir_run.cli, ['init', 'my_custom_vmargs_app', 'foreground'])
         assert result.output == ''
         assert result.exit_code == 0
+        assert my_env == {
+            'GIGALIXIR_APP_NAME': 'fake-customer-app-name', 
+            'GIGALIXIR_COMMAND': u'foreground', 
+            'PYTHONIOENCODING': 'utf-8', 
+            'GIGALIXIR_DEFAULT_VMARGS': 'false', 
+            'REPLACE_OS_VARS': 'true', 
+            'RELX_REPLACE_OS_VARS': 'true', 
+            'LIBCLUSTER_KUBERNETES_NODE_BASENAME': 'my_custom_vmargs_app', 
+            'LIBCLUSTER_KUBERNETES_SELECTOR': 'repo=my_custom_vmargs_app', 
+            'MY_POD_IP': '1.2.3.4', 
+            'DATABASE_URL': 'fake-database-url', 
+            'MY_COOKIE': 'fake-cookie', 
+            'MY_NODE_NAME': 'my_custom_vmargs_app@1.2.3.4', 
+            'ERLANG_COOKIE': 'fake-cookie', 
+            'LC_ALL': 'en_US.UTF-8', 
+            'PORT': '4000', 
+            'LOGPLEX_TOKEN': 'fake-logplex-token', 
+            # no VMARGS_PATH for custom vmargs
+            # 'VMARGS_PATH': '/release-config/vm.args'
+        }
+
         assert mock_tarfile.mock_calls == [
             mock.call('fake-customer-app-name.tar.gz', 'r:gz'),
             mock.call().extractall(),
@@ -410,74 +436,46 @@ def test_custom_vmargs(mock_tarfile, mock_os, mock_subprocess, mock_get, mock_op
         ]
 
         assert mock_os.mock_calls == [
-           mock.call.path.exists('/root/.ssh'),
-           mock.call.path.exists().__nonzero__(),
-           mock.call.path.exists('/kube-env-vars'),
-           mock.call.path.exists().__nonzero__(),
-           mock.call.getcwd(),
-           mock.call.path.expanduser('/app'),
-           # mock.call.chdir(<MagicMock name='os.path.expanduser()' id='140584589731088'>),
-           mock.call.chdir(mock.ANY),
-           # mock.call.chdir(<MagicMock name='os.getcwd()' id='140584589842768'>),
-           mock.call.chdir(mock.ANY),
-           mock.call.walk('/app'),
-           # mock.call.walk().__iter__(),
-           mock.mock._Call(('walk().__iter__', (), {})),
-           mock.call.getcwd(),
-           mock.call.path.expanduser('/app'),
-           # mock.call.chdir(<MagicMock name='os.path.expanduser()' id='140584589731088'>),
-           mock.call.chdir(mock.ANY),
-           mock.call.getcwd(),
-           # mock.call.getcwd().__str__(),
-           mock.mock._Call(('getcwd().__str__', (), {})),
-           mock.call.pipe(),
-           # mock.call.write(<Mock name='pipe_write' id='140584625038544'>, "Attempting to start 'my_app' on host '<MagicMock name='subprocess.check_output().strip()' id='140584587942160'>'\nAttempting health checks on port <MagicMock name='os.environ.get()' id='140584588716240'>\n"),
-           mock.call.write(mock.ANY, mock.ANY),
-           # mock.call.close(<Mock name='pipe_write' id='140584625038544'>),
-           mock.call.close(mock.ANY),
-           mock.call.getcwd(),
-           # mock.call.getcwd().__str__(),
-           mock.mock._Call(('getcwd().__str__', (), {})),
-           # mock.call.path.exists("<MagicMock name='os.getcwd()' id='140584589842768'>/Procfile"),
-           mock.call.path.exists(mock.ANY),
-           mock.call.path.exists().__nonzero__(),
-           # mock.call.chdir(<MagicMock name='os.getcwd()' id='140584589842768'>)
-           mock.call.chdir(mock.ANY)
+        ] + START_SSH + [
+        ] + PERSIST_ENV + [
+        ] + EXTRACT_FILE + [
+        ] + START_EPMD + [
+        ] + IS_DISTILLERY + [
+        ] + ENTER_APP_FOLDER + [
+        ] + LOG_MESSAGE + [
+        ] + IS_DISTILLERY + [
+        # not for custom_vmargs
+        # ] + GENERATE_VMARGS + [
+        ] + FOREMAN_START + [
+        ] + EXIT_APP_FOLDER + [
         ]
 
         print mock_subprocess.mock_calls
         assert mock_subprocess.mock_calls == [
-            mock.call.check_call(['/bin/bash', '-c', u"curl https://api.gigalixir.com/api/apps/%s/ssh_keys -u %s:fake-app-key | jq -r '.data | .[]' > /root/.ssh/authorized_keys" % (repo, repo)]),
+            mock.call.check_call(['/bin/bash', '-c', u"curl https://api.gigalixir.com/api/apps/my_custom_vmargs_app/ssh_keys -u my_custom_vmargs_app:fake-app-key | jq -r '.data | .[]' > /root/.ssh/authorized_keys"]),
             mock.call.Popen(['crontab'], stdin=stdin),
-            mock.call.Popen().communicate(u"* * * * * curl https://api.gigalixir.com/api/apps/%s/ssh_keys -u %s:fake-app-key | jq -r '.data | .[]' > /root/.ssh/authorized_keys && echo $(date) >> /var/log/cron.log\n" % (repo, repo)),
+            mock.call.Popen().communicate(u"* * * * * curl https://api.gigalixir.com/api/apps/my_custom_vmargs_app/ssh_keys -u my_custom_vmargs_app:fake-app-key | jq -r '.data | .[]' > /root/.ssh/authorized_keys && echo $(date) >> /var/log/cron.log\n"),
             mock.call.Popen().stdin.close(),
             mock.call.check_call(['cron']),
             mock.call.check_call(['service', 'ssh', 'start']),
             mock.call.check_output(['hostname']),
             mock.call.check_output().strip(),
-            # mock.call.check_output().strip().__str__(),
-            mock.mock._Call(('check_output().strip().__str__', (), {})),
-            # mock.call.check_output().strip().__str__(),
-            mock.mock._Call(('check_output().strip().__str__', (), {})),
-            # mock.call.check_call(['/opt/gigalixir/bin/log-shuttle', '-logs-url=http://token:@post.logs.gigalixir.com/logs', '-appname', 'my_app', '-hostname', '<MagicMock', "name='subprocess.check_output().strip()'", "id='139706507347280'>", '-procid', 'gigalixir-run'], stdin=<Mock name='pipe_read' id='139706509973264'>),
-            mock.call.check_call(['/opt/gigalixir/bin/log-shuttle', '-logs-url=http://token:@post.logs.gigalixir.com/logs', '-appname', repo, '-hostname', mock.ANY, mock.ANY, mock.ANY, '-procid', 'gigalixir-run'], stdin=pipe_read),
-            # mock.call.check_output().strip().__str__(),
-            mock.mock._Call(('check_output().strip().__str__', (), {})),
-            # mock.call.check_output().strip().__str__(),
-            mock.mock._Call(('check_output().strip().__str__', (), {})),
+            mock.mock._Call(('check_output().strip().__unicode__', (), {})),
+            mock.mock._Call(('check_output().strip().__unicode__', (), {})),
+            mock.call.check_call(['/opt/gigalixir/bin/log-shuttle', '-logs-url=http://token:fake-logplex-token@post.logs.gigalixir.com/logs', '-appname', 'my_custom_vmargs_app', '-hostname', mock.ANY, mock.ANY, mock.ANY, '-procid', 'gigalixir-run'], stdin=pipe_read),
             mock.call.Popen(['foreman', 'start', '-d', '.', '--color', '--no-timestamp', '-f', 'Procfile'], stdout=stdin),
-            # mock.call.check_call(['/opt/gigalixir/bin/log-shuttle', '-logs-url=http://token:@post.logs.gigalixir.com/logs', '-appname', 'my_app', '-hostname', '<MagicMock', "name='subprocess.check_output().strip()'", "id='140316095358288'>", '-procid', '<MagicMock', "name='subprocess.check_output().strip()'", "id='140316095358288'>", '-num-outlets', '1', '-batch-size=5', '-back-buff=5000'], stdin=<MagicMock name='subprocess.Popen().stdout' id='140316095582800'>),
+            mock.mock._Call(('check_output().strip().__unicode__', (), {})),
+            mock.mock._Call(('check_output().strip().__unicode__', (), {})),
             # -hostname and -procid have 3 mock.ANYs behind it because
             # the string representation of MagicMock is broken into
             # 3 strings.. ugly.
-            mock.call.check_call(['/opt/gigalixir/bin/log-shuttle', '-logs-url=http://token:@post.logs.gigalixir.com/logs', '-appname', repo, '-hostname', mock.ANY, mock.ANY, mock.ANY, '-procid', mock.ANY, mock.ANY, mock.ANY, '-num-outlets', '1', '-batch-size=5', '-back-buff=5000'], stdin=mock.ANY),
+            mock.call.check_call(['/opt/gigalixir/bin/log-shuttle', '-logs-url=http://token:fake-logplex-token@post.logs.gigalixir.com/logs', '-appname', 'my_custom_vmargs_app', '-hostname', mock.ANY, mock.ANY, mock.ANY, '-procid', mock.ANY, mock.ANY, mock.ANY, '-num-outlets', '1', '-batch-size=5', '-back-buff=5000'], stdin=mock.ANY),
             mock.call.Popen().wait()
         ]
 
         assert mock_get.mock_calls == [
-            mock.call(u'https://api.gigalixir.com/api/apps/%s/releases/current' % repo, auth=(repo, u'fake-app-key')),
+            mock.call(u'https://api.gigalixir.com/api/apps/my_custom_vmargs_app/releases/current', auth=(u'my_custom_vmargs_app', u'fake-app-key')),
             mock.call('https://storage.googleapis.com/slug-bucket/production/sunny-wellgroomed-africanpiedkingfisher/releases/0.0.2/SHA/gigalixir_getting_started.tar.gz', stream=True),
-            mock.call('https://api.gigalixir.com/api/apps/%s/releases/current' % repo, auth=(repo, 'fake-app-key'))
         ]
 
         assert mock_open.mock_calls == [
@@ -488,32 +486,13 @@ def test_custom_vmargs(mock_tarfile, mock_os, mock_subprocess, mock_get, mock_op
             mock.call('/kube-env-vars/APP_KEY', 'w'),
             mock.call('/kube-env-vars/LOGPLEX_TOKEN', 'w'),
             mock.call('/app/fake-customer-app-name.tar.gz', 'wb'),
-            mock.call('/kube-env-vars/MY_POD_IP', 'r'),
-            mock.call('/kube-env-vars/ERLANG_COOKIE', 'r'),
-            mock.call('/kube-env-vars/REPO', 'r'),
-            mock.call('/kube-env-vars/APP_KEY', 'r'),
-            mock.call('/kube-env-vars/APP', 'r'),
-            mock.call('/kube-env-vars/LOGPLEX_TOKEN', 'r'),
-        ]
 
-        assert my_env == {
-            'PYTHONIOENCODING': 'utf-8', 
-            'GIGALIXIR_DEFAULT_VMARGS': 'false', 
-            'REPLACE_OS_VARS': 'true', 
-            'RELX_REPLACE_OS_VARS': 'true', 
-            'LIBCLUSTER_KUBERNETES_NODE_BASENAME': repo, 
-            'LIBCLUSTER_KUBERNETES_SELECTOR': 'repo=%s' % repo, 
-            'MY_POD_IP': '1.2.3.4', 
-            'GIGALIXIR_COMMAND': u'foreground', 
-            'DATABASE_URL': 'fake-database-url', 
-            'MY_COOKIE': 'fake-cookie', 
-            'MY_NODE_NAME': '%s@1.2.3.4' % repo, 
-            'GIGALIXIR_APP_NAME': 'fake-customer-app-name', 
-            'ERLANG_COOKIE': 'fake-cookie', 
-            'LC_ALL': 'en_US.UTF-8', 
-            'PORT': '4000', 
-            'LOGPLEX_TOKEN': '', 
-        }
+            # not for custom_vmargs
+            # generate_vmargs
+            # mock.call(<MagicMock name='os.path.join()' id='139897244298064'>, 'r'),
+            # mock.call(mock.ANY, 'r'),
+            # mock.call('/release-config/vm.args', 'w'),
+        ]
 
 @mock.patch('gigalixir_run.open', side_effect=mocked_open_fn("my_app"))
 @mock.patch('requests.get', side_effect=mocked_requests_get)
