06dfc2cef5a58fe765dda9cc6377dcc509d50dee
Jesse <jesse@gigalixir.com>
2021-01-25 11:59:04 -0800

    use python3.8 finally. gives us more time and is also easier turns out becuase most dependencies expect newer versions and do weird things on python 3.5 which is not widely supported anymore. tested with mix-16 and mix-18 so far. need to test all other scenarios.

--- setup.py
+++ setup.py
@@ -13,10 +13,18 @@ setup(
         'requests~=2.13.0',
         'rollbar~=0.13.11',
 
+        # needed for requests, but why isn't it installed through requests's dependencies? maybe upgrading requests will fix this and then we can remove these lines
+        # something to do with this?
+        # https://github.com/psf/requests/blob/v2.13.0/setup.py#L98
+        # cryptography comes from click
+        'idna==3.1',
+        'pyOpenSSL==20.0.1',
+
         # I guess this is required to peg six to 1.9.0 
         # which rollbar requires.
         # TODO: after upgrading to python3, remove this and upgrade rollbar
         'six~=1.9.0',
+
         'pystache~=0.5.4',
     ],
     entry_points='''
@@ -32,7 +40,7 @@ setup(
 
             # needed to avoid error when installing, see
             # https://stackoverflow.com/questions/34819221/why-is-python-setup-py-saying-invalid-command-bdist-wheel-on-travis-ci
-            'wheel',
+            # 'wheel',
         ],
     }
 
